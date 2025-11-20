7df81ea179c77a08624b8e9d21778f848750020c
Thomas <thomas.albuquerqueeu@gmail.com>
2025-11-20 19:22:04 -0300

    Primeiro commit: Sistema de Compras com Cartão de Crédito

---
+++ CartaoDeCredito.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite; // Inicialmente, o saldo é igual ao limite
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compra compra) {
+        if (this.saldo >= compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+}
